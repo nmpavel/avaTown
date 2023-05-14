38b1ab2bdebb26facb52c043142486617b9987d2
nmpavel <pavel.mohammad@vivacomsolutions.com>
2023-05-15 02:59:14 +0600

    Design Issue Solved

--- src/components/OtherProducts.tsx
+++ src/components/OtherProducts.tsx
@@ -9,7 +9,7 @@ const OtherProducts = () => {
 
     const newArray = avatars.filter(obj => obj.id !== productId);
   return (
-    <div className=' relative top-16 left-16 bg-white px-10 mt-10 rounded-xl h-[1390px] overflow-x-hidden overflow-y-auto'>
+    <div className=' relative top-16 left-6 bg-white px-10 mt-10 rounded-xl h-[1390px] overflow-x-hidden overflow-y-auto'>
         <h1 className='text-2xl font-bold py-4'>Other Products</h1>
         {
             newArray.map((avatar)=>{
